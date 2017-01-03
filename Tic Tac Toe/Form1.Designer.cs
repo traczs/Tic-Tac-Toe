e7d17288d34dcd462fb296af264722129d2e8887
Samuel Tracz <samueldominik@gmail.com>
2017-01-03 16:38:09 -0500

    TicTacToe
    Game of tic-tac-toe

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(95, 96);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(153, 133);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseClick);
+            this.btn1.MouseEnter += new System.EventHandler(this.btn1_MouseEnter);
+            this.btn1.MouseLeave += new System.EventHandler(this.btn1_MouseLeave);
+            // 
+            // btn2
+            // 
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(256, 96);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(153, 133);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn2_MouseClick);
+            this.btn2.MouseEnter += new System.EventHandler(this.btn2_MouseEnter);
+            this.btn2.MouseLeave += new System.EventHandler(this.btn2_MouseLeave);
+            // 
+            // btn3
+            // 
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(417, 96);
+            this.btn3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(153, 133);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn3_MouseClick);
+            this.btn3.MouseEnter += new System.EventHandler(this.btn3_MouseEnter);
+            this.btn3.MouseLeave += new System.EventHandler(this.btn3_MouseLeave);
+            // 
+            // btn4
+            // 
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(95, 236);
+            this.btn4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(153, 133);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn4_MouseClick);
+            this.btn4.MouseEnter += new System.EventHandler(this.btn4_MouseEnter);
+            this.btn4.MouseLeave += new System.EventHandler(this.btn4_MouseLeave);
+            // 
+            // btn5
+            // 
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(256, 236);
+            this.btn5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(153, 133);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn5_MouseClick);
+            this.btn5.MouseEnter += new System.EventHandler(this.btn5_MouseEnter);
+            this.btn5.MouseLeave += new System.EventHandler(this.btn5_MouseLeave);
+            // 
+            // btn6
+            // 
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(417, 236);
+            this.btn6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(153, 133);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn6_MouseClick);
+            this.btn6.MouseEnter += new System.EventHandler(this.btn6_MouseEnter);
+            this.btn6.MouseLeave += new System.EventHandler(this.btn6_MouseLeave);
+            // 
+            // btn7
+            // 
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(95, 377);
+            this.btn7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(153, 133);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn7_MouseClick);
+            this.btn7.MouseEnter += new System.EventHandler(this.btn7_MouseEnter);
+            this.btn7.MouseLeave += new System.EventHandler(this.btn7_MouseLeave);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(256, 377);
+            this.btn8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(153, 133);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn8_MouseClick);
+            this.btn8.MouseEnter += new System.EventHandler(this.btn8_MouseEnter);
+            this.btn8.MouseLeave += new System.EventHandler(this.btn8_MouseLeave);
+            // 
+            // btn9
+            // 
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(417, 377);
+            this.btn9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(153, 133);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn9_MouseClick);
+            this.btn9.MouseEnter += new System.EventHandler(this.btn9_MouseEnter);
+            this.btn9.MouseLeave += new System.EventHandler(this.btn9_MouseLeave);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(249, 37);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(173, 31);
+            this.lblTitle.TabIndex = 9;
+            this.lblTitle.Text = "Tic-Tac-Toe";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.Location = new System.Drawing.Point(725, 126);
+            this.lblTurn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(46, 17);
+            this.lblTurn.TabIndex = 10;
+            this.lblTurn.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 606);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTurn;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        //bool x and o will be checked to see which turn it is
+        bool x = true;
+        bool o = false;
+        //bool clicked will check if the buttons were pressed
+        bool clicked1, clicked2, clicked3, clicked4, clicked5, clicked6, clicked7, clicked8, clicked9 = false;
+        int one, two, three, four, five, six, seven, eight, nine;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lblTurn.Text = "Player X turn";
+        }
+
+        //mouse enter shows symbol on button
+        //checks which player symbol should appear and if it was previously clicked
+        #region
+        private void btn1_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked1 == false)
+            {
+                btn1.Text = "X";
+            }
+            else if (o == true && x == false && clicked1 == false)
+            {
+                btn1.Text = "O";
+            }
+        }
+
+        private void btn2_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked2 == false)
+            {
+                btn2.Text = "X";
+            }
+            else if (o == true && x == false && clicked2 == false)
+            {
+                btn2.Text = "O";
+            }
+        }
+
+        private void btn3_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked3 == false)
+            {
+                btn3.Text = "X";
+            }
+            else if (o == true && x == false && clicked3 == false)
+            {
+                btn3.Text = "O";
+            }
+        }
+
+        private void btn4_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked4 == false)
+            {
+                btn4.Text = "X";
+            }
+            else if (o == true && x == false && clicked4 == false)
+            {
+                btn4.Text = "O";
+            }
+        }
+
+        private void btn5_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked5 == false)
+            {
+                btn5.Text = "X";
+            }
+            else if (o == true && x == false && clicked5 == false)
+            {
+                btn5.Text = "O";
+            }
+        }
+
+        private void btn6_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked6 == false)
+            {
+                btn6.Text = "X";
+            }
+            else if (o == true && x == false && clicked6 == false)
+            {
+                btn6.Text = "O";
+            }
+        }
+
+        private void btn7_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked7 == false)
+            {
+                btn7.Text = "X";
+            }
+            else if (o == true && x == false && clicked7 == false)
+            {
+                btn7.Text = "O";
+            }
+        }
+
+        private void btn8_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked8 == false)
+            {
+                btn8.Text = "X";
+            }
+            else if (o == true && x == false && clicked8 == false)
+            {
+                btn8.Text = "O";
+            }
+        }
+
+        private void btn9_MouseEnter(object sender, EventArgs e)
+        {
+            if (x == true && o == false && clicked9 == false)
+            {
+                btn9.Text = "X";
+            }
+            else if (o == true && x == false && clicked9 == false)
+            {
+                btn9.Text = "O";
+            }
+        }
+        #endregion
+
+        //mouse leave causes the symbols to disappear if button was not clicked
+        #region
+        private void btn1_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked1 == false)
+            {
+                btn1.Text = "";
+            }
+        }
+
+        private void btn2_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked2 == false)
+            {
+                btn2.Text = "";
+            }
+        }
+
+        private void btn3_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked3 == false)
+            {
+                btn3.Text = "";
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked4 == false)
+            {
+                btn4.Text = "";
+            }
+        }
+
+        private void btn5_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked5 == false)
+            {
+                btn5.Text = "";
+            }
+        }
+
+        private void btn6_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked6 == false)
+            {
+                btn6.Text = "";
+            }
+        }
+
+        private void btn7_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked7 == false)
+            {
+                btn7.Text = "";
+            }
+        }
+
+        private void btn8_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked8 == false)
+            {
+                btn8.Text = "";
+            }
+        }
+
+        private void btn9_MouseLeave(object sender, EventArgs e)
+        {
+            if (clicked9 == false)
+            {
+                btn9.Text = "";
+            }
+        }
+        #endregion
+
+        //checks the click and shows the appropriate symbol when clicked
+        //once clicked, button cannot be clicked again
+        #region
+        private void btn1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+            if (x == true)
+            {
+                btn1.Text = "X";
+                clicked1 = true;
+                one = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn1.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn1.Text = "O";
+                clicked1 = true;
+                one = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn1.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn2.Text = "X";
+                clicked2 = true;
+                two = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn2.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn2.Text = "O";
+                clicked2 = true;
+                two = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn2.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn3_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn3.Text = "X";
+                clicked3 = true;
+                three = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn3.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn3.Text = "O";
+                clicked3 = true;
+                three = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn3.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn4_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn4.Text = "X";
+                clicked4 = true;
+                four = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn4.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn4.Text = "O";
+                clicked4 = true;
+                four = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn4.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn5_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn5.Text = "X";
+                clicked5 = true;
+                five = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn5.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn5.Text = "O";
+                clicked5 = true;
+                five = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn5.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn6_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn6.Text = "X";
+                clicked6 = true;
+                six = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn6.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn6.Text = "O";
+                clicked6 = true;
+                six = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn6.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn7_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn7.Text = "X";
+                clicked7 = true;
+                seven = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn7.Enabled = false; 
+                check();
+            }
+            else if (o == true)
+            {
+                btn7.Text = "O";
+                clicked7 = true;
+                seven = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn7.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn8_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn8.Text = "X";
+                clicked8 = true;
+                eight = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn8.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn8.Text = "O";
+                clicked8 = true;
+                eight = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn8.Enabled = false;
+                check();
+            }
+        }
+
+        private void btn9_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (x == true)
+            {
+                btn9.Text = "X";
+                clicked9 = true;
+                nine = 1;
+                x = false;
+                o = true;
+                lblTurn.Text = "Player O turn";
+                btn9.Enabled = false;
+                check();
+            }
+            else if (o == true)
+            {
+                btn9.Text = "O";
+                clicked9 = true;
+                nine = 2;
+                o = false;
+                x = true;
+                lblTurn.Text = "Player X turn";
+                btn9.Enabled = false;
+                check();
+            }
+        }
+        #endregion
+
+        //check for winning row, or if no winning row, it declares a tie
+        private void check()
+        {
+            if ((one==1 && two==1 && three==1)||(one==1 && four==1 && seven==1)||(two==1 && five==1 && eight==1)||(three==1 && six==1 && nine==1)||(four==1 && five==1 && six==1)||(seven==1 && eight==1 && nine==1)||(one==1 && five==1 && nine==1)||(three==1 && five==1 && seven==1))
+            {
+                DialogResult win = MessageBox.Show("Player X wins! Play again?", "Play again?", MessageBoxButtons.YesNo );
+                if (win == DialogResult.Yes)
+                {
+                    
+                    Application.Restart();
+                }
+                else if (win == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if ((one == 2 && two == 2 && three == 2) || (one == 2 && four == 2 && seven == 2) || (two == 2 && five == 2 && eight == 2) || (three == 2 && six == 2 && nine == 2) || (four == 2 && five == 2 && six == 2) || (seven == 2 && eight == 2 && nine == 2) || (one == 2 && five == 2 && nine == 2) || (three == 2 && five == 2 && seven == 2))
+            {
+                DialogResult win = MessageBox.Show("Player O wins! Play again? ", "Play again?", MessageBoxButtons.YesNo);
+                if (win == DialogResult.Yes)
+                {
+                    
+                    Application.Restart();
+                }
+                else if (win == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+            else if (btn1.Enabled == false && btn2.Enabled == false && btn3.Enabled == false && btn4.Enabled == false && btn5.Enabled == false && btn6.Enabled == false && btn7.Enabled == false && btn8.Enabled == false && btn9.Enabled ==false)
+            {
+                DialogResult win = MessageBox.Show("Tie! Play again? ", "Play again?", MessageBoxButtons.YesNo);
+                if (win == DialogResult.Yes)
+                {
+                    
+                    Application.Restart();
+                }
+                else if (win == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        
+    }
+}
